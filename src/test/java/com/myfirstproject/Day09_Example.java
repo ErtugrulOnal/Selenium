904b46cfdf832678b9f82afcc43c5b85297d7fdf
ErtugrulOnal <103278611+ErtugrulOnal@users.noreply.github.com>
2023-05-12 20:11:25 +0200

    V8

---
+++ src/test/java/com/myfirstproject/Day09_Example.java
@@ -0,0 +1,28 @@
+package com.myfirstproject;
+
+import com.myfirstproject.utilities.TestBase;
+import org.junit.Test;
+
+public class Day09_Example extends TestBase {
+    /*
+    Given go to file:///C:/Users/TechProEd/Desktop/Html-Css%20Batch132-133/index.html
+    Click on "Click Here For Regisration Form"
+    Enter username: John
+    Enter password: John.123
+    Fill the form
+    Click Confirm
+     */
+
+    @Test
+    public void test(){
+//        Given go to file:///C:/Users/TechProEd/Desktop/Html-Css%20Batch132-133/index.html
+//        Click on "Click Here For Regisration Form"
+//        Enter username: John
+//        Enter password: John.123
+//        Fill the form
+//        Click Confirm
+
+
+
+    }
+}
